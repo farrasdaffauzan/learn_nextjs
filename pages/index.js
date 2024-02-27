1d43728a9e7b58540cd72e05cdb90b20b6666128
farrasdaffauzan <farrasdaffauzan@gmail.com>
2024-02-27 23:07:50 +0700

    Tugas hari ke-6

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from "@/layout";
 export default function Main() {
   return (
     <>
-      <Layout>
+      <Layout metatitle={"home"}>
         <p>Home</p>
       </Layout>
     </>
--- pages/profile.js
+++ pages/profile.js
@@ -3,7 +3,7 @@ import Layout from "@/layout";
 export default function Profile() {
   return (
     <>
-      <Layout>
+      <Layout metatitle={"Profile"}>
         <p>Profile</p>
       </Layout>
     </>
